16dfc4c877dfcbae64562685b9d91101e5e0c13a
Henner R Setyono <henner.renardi@gmail.com>
2022-05-09 21:42:15 +0700

    Combined all PDFs

--- src/exportAssets.js
+++ src/exportAssets.js
@@ -1,107 +1,77 @@
 const application = require("application");
-const fs          = require("uxp").storage.localFileSystem;
+const { localFileSystem, formats } = require("uxp").storage;
+const { PDFDocument } = require('pdf-lib');
 
-module.exports = async function exportAssets( selection, root, scale, format )
-{
-    console.log('selection:');
-    console.log( selection.items );
-    //you need an actual selection
+module.exports = async function exportAssets(selection, root) {
     if (selection.items.length <= 0) {
         return;
     }
 
-    scale  = scale.replace(/[^\d.-]/g, '');
-    scale  = Number(scale);
-
-    if ( format == undefined )
-    {
-        format = "png";
-    }
-
-    var exportSettings = {};
-
-    switch( format )
-    {
-        case "png":
-            //PNG settings
-            exportSettings.type        = application.RenditionType.PNG;
-            exportSettings.quality     = null;
-            exportSettings.scale       = scale;
-            exportSettings.minify      = null;
-            exportSettings.embedImages = null;
-        break;
-
-        case "jpg":
-            //JPG settings
-            exportSettings.type        = application.RenditionType.JPG;
-            exportSettings.quality     = 100;
-            exportSettings.scale       = scale;
-            exportSettings.minify      = null;
-            exportSettings.embedImages = null;
-        break;
-
-        case "svg":
-            //SVG settings
-            exportSettings.type        = application.RenditionType.SVG;
-            exportSettings.scale       = null;
-            exportSettings.quality      = null;
-            exportSettings.minify      = true;
-            exportSettings.embedImages = true;
-        break
-
-        case "pdf":
-            //PDF settings
-            exportSettings.type        = application.RenditionType.PDF;
-            exportSettings.scale       = null;
-            exportSettings.quality     = null;
-            exportSettings.minify      = null;
-            exportSettings.embedImages = null;
-        break;
-
-        default :
-            //PNG settings
-            exportSettings.type        = application.RenditionType.PNG;
-            exportSettings.quality     = null;
-            exportSettings.scale       = scale;
-            exportSettings.minify      = null;
-            exportSettings.embedImages = null;
-    }
-
-    console.log(exportSettings);
-
     //set up file I/O
-    const folder = await fs.getFolder();
+    const folder = await localFileSystem.getFolder();
 
     const files = [];
-    for ( var i = 0; i<selection.items.length; i++ )
-    {
-        const file = await folder.createFile((selection.items[i].name.replace(/[^a-z0-9]/gi, '_').toLowerCase() + "." + format), {overwrite:true} );
+    for (var i = 0; i<selection.items.length; i++) {
+        selection.items[i].filename = selection.items[i].name.replace(/[^a-z0-9]/gi, '_').toLowerCase() + '.pdf';
+        const file = await folder.createFile(
+            selection.items[i].filename,
+            { overwrite:true }
+        );
         files.push(file);
     }
 
     //set up renditions array
     const renditions = [];
-    for ( var k = 0; k<selection.items.length; k++ )
-    {
+    for (var k = 0; k<selection.items.length; k++) {
         renditions.push({
             node: selection.items[k],
             outputFile: files[k],
-            type: exportSettings.type,
-            scale: exportSettings.scale,
-            quality: exportSettings.quality,
-            minify: exportSettings.minify,
-            embedImages: exportSettings.embedImages,
+            type: application.RenditionType.PDF,
+            scale: null,
+            quality: null,
+            minify: null,
+            embedImages: null,
         });
     }
 
     //use application class to print out assets,
     //return render result.
-    return application.createRenditions(renditions)
-        .then(results => {
-            return true;
-        })
-        .catch(error => {
-            //console.log(error);
-            return false;
-        })
+    try {
+        const results = await application.createRenditions(renditions);
+        const mergedPDF = await folder.createFile(
+            selection.items[0].filename,
+            { overwrite: true }
+        );
+        
+        const buffers = [];
+        for (const r of results) {
+            const buffer = await r.outputFile.read({
+                format: formats.binary
+            });
+            buffers.push(buffer);
+        }
+
+        // PDFLib
+        const tempPDF = await PDFDocument.create();
+        for (const b of buffers) { 
+            const pdf = await PDFDocument.load(b);
+            const copiedPages = await tempPDF.copyPages(pdf, pdf.getPageIndices());
+            copiedPages.forEach((page) => {
+                tempPDF.addPage(page); 
+            });
+        }
+
+        const mergedBuffer = await tempPDF.save(); 
+        await mergedPDF.write(mergedBuffer);
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+
+    // const merger = new PDFMerger();
+    // for(const file of files) {
+    //     await merger.add(file);
+    // }
+    // await merger.save('merged.pdf');
 }
--- src/main.jsx
+++ src/main.jsx
@@ -23,42 +23,6 @@ const exportAssets = require("./exportAssets");
 class UI extends React.Component {
     constructor(props) {
         super(props);
-
-        //reference to the select node
-        //so we can force a value into
-        //it once the component is mounted..
-        this.mySelect = React.createRef();
-
-        this.state = { scale: props.scale || "1x",
-                       format: props.format || "png",
-                       notScalableFormat: false, //this controls UI input on/off
-        };
-
-        this.onInputScaleChange = (e) => {
-            this.setState({ scale: e.target.value })
-        }
-
-        this.onSelectFormatChange = (e) => {
-            var isFormatNotScalable;
-            switch( this.mySelect.current.value )
-            {
-                case "png":
-                    isFormatNotScalable = false;
-                break;
-                case "jpg":
-                    isFormatNotScalable = false;
-                break;
-                case "svg":
-                    isFormatNotScalable = true;
-                break;
-                case "pdf":
-                    isFormatNotScalable = true;
-                break;
-            }
-            this.setState( { format: this.mySelect.current.value,
-                             notScalableFormat: isFormatNotScalable })
-        }
-
         this.onExportClick = (e) => {
             //default behaviour
             //closes the dialog right away.
@@ -74,15 +38,10 @@ class UI extends React.Component {
         }
 
         this.export = async() => {
-          console.log('exporting..');
-          //console.log('items: ' + this.props.selection.items.length);
-          //console.log('scale: ' + this.state.scale);
-          //console.log('format: ' + this.state.format);
-
-          var exp = await exportAssets( this.props.selection,
-                                        this.props.root,
-                                        this.state.scale,
-                                        this.state.format ); //true | false
+            await exportAssets(
+                this.props.selection,
+                this.props.root
+            );
 
           this.props.dialog.close();
         }
@@ -91,13 +50,13 @@ class UI extends React.Component {
     //on mount, let's force the default value
     //into our format dropdown.
     componentDidMount() {
-        //a reference to the node becomes accessible
-        //at the "current" attribute of the ref.
-        this.mySelect.current.value = "png";
     }
 
     //render dialog UI
     render() {
+        // Abort if no selection
+        if (this.props.selection.items.length <= 0) { return; }
+
         return (
             <form id="custom-exporter">
 
@@ -106,30 +65,10 @@ class UI extends React.Component {
                         <div className="logo"></div>
                     </div>
                     <div className="title">
-                        <h1>Custom Export</h1>
-                        <p>Export selected layers and artboards to any scale.</p>
+                        <h1>Export</h1>
+                        <p>Create a PDF from selected layers</p>
                     </div>
                 </div>
-
-                <div className="controls">
-                    <label>
-                        <span>Scale</span>
-                        <input value={this.state.scale} disabled={this.state.notScalableFormat} onChange={this.onInputScaleChange} />
-                    </label>
-
-                    <label>
-                        <span>Format</span>
-                        <select ref={this.mySelect}
-                                value={this.state.format}
-                                onChange={this.onSelectFormatChange}>
-                            <option value="png">png</option>
-                            <option value="jpg">jpg</option>
-                            <option value="svg">svg</option>
-                            <option value="pdf">pdf</option>
-                        </select>
-                    </label>
-                </div>
-
                 <footer>
                     <button uxp-variant="primary"
                             onClick={this.onCancelClick}>Cancel</button>
